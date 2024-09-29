bb878a2a90f7c66e3a8a7ed4cc7808507f0c5440
Seán Kenneally <Ctw20.sean.kenneally@corketb.ie>
2024-09-29 17:50:44 +0100

    Add files via upload

---
+++ p42 tasks 1+2.py
@@ -0,0 +1,41 @@
+#Sean Kenneally
+#29/9/24
+#P42 Tasks 1+2
+
+#Task 1
+n1 = int(input("Insert a number:"))
+n2 = int(input("Insert a number:"))
+n3 = int(input("Insert a number:"))
+n4 = int(input("Insert a number:"))
+n5 = int(input("Insert a number:"))
+
+sN1 = n1+1
+sN2 = n2+1
+sN3 = n3+1
+sN4 = n4+1
+sN5 = n5+1
+
+numberList = [sN1, sN2, sN3, sN4, sN5]
+print(numberList)
+print("\n")
+
+#Task 2
+hour1 = int(input("Insert hour:"))
+hour2 = int(input("Insert hour:"))
+hour3 = int(input("Insert hour:"))
+hour4 = int(input("Insert hour:"))
+hour5 = int(input("Insert hour:"))
+hour6 = int(input("Insert hour:"))
+hour7 = int(input("Insert hour:"))
+
+milk1 = hour1*0.5
+milk2 = hour2*0.5
+milk3 = hour3*0.5
+milk4 = hour4*0.5
+milk5 = hour5*0.5
+milk6 = hour6*0.5
+milk7 = hour7*0.5
+
+milkWeek = [milk1, milk2, milk3, milk4, milk5, milk6, milk7]
+print(milkWeek)
+
